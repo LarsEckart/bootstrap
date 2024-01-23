637b9ad2502740816435efeaeac0aab79d7e8132
Lars Eckart <lars.eckart@hey.com>
2024-01-23 14:44:03 +0200

    shameless green

--- src/test/java/kata/PlayingCards.java
+++ src/test/java/kata/PlayingCards.java
@@ -3,17 +3,28 @@ package kata;
 import kata.position.Position;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 final class PlayingCards {
     private final List<Card> cards;
+    private final Set<Position> excludedPositions = new HashSet<>();
 
     public PlayingCards() {
         this.cards = new ArrayList<Card>();
     }
 
     public int score() {
-        return cards.stream().filter(Card::flipped).mapToInt(Card::value).sum();
+        int sum = 0;
+        for (int i = 0; i < cards.size(); i++) {
+            Card card = cards.get(i);
+            if (card.flipped() && !excludedPositions.contains(Position.fromIndex(i))) {
+                int value = card.value();
+                sum += value;
+            }
+        }
+        return sum;
     }
 
     public int numberOfCards() {
@@ -31,6 +42,11 @@ final class PlayingCards {
     public Card swap(Position position, Card newCard) {
         Card card = cards.get(position.toIndex());
         cards.set(position.toIndex(), newCard);
+        if (this.cards.get(0).value() == this.cards.get(4).value() && this.cards.get(4).value() == this.cards.get(8).value()) {
+            excludedPositions.add(Position.fromIndex(0));
+            excludedPositions.add(Position.fromIndex(4));
+            excludedPositions.add(Position.fromIndex(8));
+        }
         return card;
     }
 
