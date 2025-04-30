b529aed698b1fec033257ac2b2bb241980baf9cc
Lars Eckart <lars.eckart@hey.com>
2025-04-30 21:47:54 +0300

    prompt 10.1 and fixes to the ContractTest

--- src/test/java/kata/cache/CacheContractTest.java
+++ src/test/java/kata/cache/CacheContractTest.java
@@ -13,8 +13,8 @@ abstract class CacheContractTest {
   class key_value_contract {
     @Test
     void put_and_get_returns_value() throws Exception {
-      var key = new CacheKey("foo");
-      var value = new CacheValue("bar");
+      var key = new CacheKey("key1");
+      var value = new CacheValue("value1");
 
       cache().put(key, value);
       var result = cache().get(key);
@@ -24,7 +24,7 @@ abstract class CacheContractTest {
 
     @Test
     void get_returns_empty_optional_when_key_missing() throws Exception {
-      var key = new CacheKey("missing");
+      var key = new CacheKey("missing_key");
 
       var result = cache().get(key);
 
@@ -33,8 +33,8 @@ abstract class CacheContractTest {
 
     @Test
     void getOrThrow_returns_value_when_present() throws Exception {
-      var key = new CacheKey("foo");
-      var value = new CacheValue("bar");
+      var key = new CacheKey("key2");
+      var value = new CacheValue("value2");
 
       cache().put(key, value);
       var result = cache().getOrThrow(key);
@@ -44,7 +44,7 @@ abstract class CacheContractTest {
 
     @Test
     void getOrThrow_throws_when_key_missing() {
-      var key = new CacheKey("missing");
+      var key = new CacheKey("missing_key2");
 
       assertThatThrownBy(() -> cache().getOrThrow(key))
           .isInstanceOf(RuntimeException.class)
@@ -53,8 +53,8 @@ abstract class CacheContractTest {
 
     @Test
     void remove_deletes_key() throws Exception {
-      var key = new CacheKey("foo");
-      var value = new CacheValue("bar");
+      var key = new CacheKey("key3");
+      var value = new CacheValue("value3");
 
       cache().put(key, value);
       cache().remove(key);
@@ -65,8 +65,8 @@ abstract class CacheContractTest {
 
     @Test
     void exists_returns_true_if_key_present() throws Exception {
-      var key = new CacheKey("foo");
-      var value = new CacheValue("bar");
+      var key = new CacheKey("key4");
+      var value = new CacheValue("value4");
 
       cache().put(key, value);
       var result = cache().exists(key);
@@ -76,7 +76,7 @@ abstract class CacheContractTest {
 
     @Test
     void exists_returns_false_if_key_missing() throws Exception {
-      var key = new CacheKey("missing");
+      var key = new CacheKey("missing_key3");
 
       var result = cache().exists(key);
 
@@ -88,9 +88,9 @@ abstract class CacheContractTest {
   class hash_contract {
     @Test
     void hset_and_hget_returns_value() throws Exception {
-      var hash = new HashName("h");
-      var field = new HashField("f");
-      var value = new CacheValue("v");
+      var hash = new HashName("hash1");
+      var field = new HashField("field1");
+      var value = new CacheValue("hash_value1");
 
       cache().hset(hash, field, value);
       var result = cache().hget(hash, field);
@@ -100,8 +100,8 @@ abstract class CacheContractTest {
 
     @Test
     void hget_returns_empty_optional_when_hash_or_field_missing() throws Exception {
-      var hash = new HashName("h");
-      var field = new HashField("f");
+      var hash = new HashName("missing_hash");
+      var field = new HashField("missing_field");
 
       var result = cache().hget(hash, field);
 
@@ -110,9 +110,9 @@ abstract class CacheContractTest {
 
     @Test
     void hgetOrThrow_returns_value_when_present() throws Exception {
-      var hash = new HashName("h");
-      var field = new HashField("f");
-      var value = new CacheValue("v");
+      var hash = new HashName("hash2");
+      var field = new HashField("field2");
+      var value = new CacheValue("hash_value2");
 
       cache().hset(hash, field, value);
       var result = cache().hgetOrThrow(hash, field);
@@ -122,8 +122,8 @@ abstract class CacheContractTest {
 
     @Test
     void hgetOrThrow_throws_when_hash_missing() {
-      var hash = new HashName("h");
-      var field = new HashField("f");
+      var hash = new HashName("missing_hash2");
+      var field = new HashField("missing_field2");
 
       assertThatThrownBy(() -> cache().hgetOrThrow(hash, field))
           .isInstanceOf(RuntimeException.class)
@@ -132,9 +132,9 @@ abstract class CacheContractTest {
 
     @Test
     void hgetOrThrow_throws_when_field_missing() throws Exception {
-      var hash = new HashName("h");
-      var field = new HashField("f");
-      cache().hset(hash, new HashField("other"), new CacheValue("v"));
+      var hash = new HashName("hash3");
+      var field = new HashField("missing_field3");
+      cache().hset(hash, new HashField("other_field"), new CacheValue("hash_value3"));
 
       assertThatThrownBy(() -> cache().hgetOrThrow(hash, field))
           .isInstanceOf(RuntimeException.class)
@@ -143,9 +143,9 @@ abstract class CacheContractTest {
 
     @Test
     void hdel_removes_field() throws Exception {
-      var hash = new HashName("h");
-      var field = new HashField("f");
-      var value = new CacheValue("v");
+      var hash = new HashName("hash4");
+      var field = new HashField("field4");
+      var value = new CacheValue("hash_value4");
 
       cache().hset(hash, field, value);
       cache().hdel(hash, field);
